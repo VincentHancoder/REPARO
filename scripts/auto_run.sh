f676329a33b0588f23311b88e237192cac74dc90
Ray Yang <73805072+Yangr116@users.noreply.github.com>
2024-09-14 10:39:08 +0800

    Update auto_run.sh

--- scripts/auto_run.sh
+++ scripts/auto_run.sh
@@ -2,7 +2,7 @@ NUM_GPUS=8
 
 CONFIG=config/dreamgaussian/furniture_rgbxy_wo_proxy.json
 #python evaluation/eval_2d.py --config $CONFIG
- bash scripts/run.sh $CONFIG $NUM_GPUS
+bash scripts/run.sh $CONFIG $NUM_GPUS
 # CONFIG=config/triposr/triposr_furniture_rgb_wo_proxy.json
 # python evaluation/eval_2d.py --config $CONFIG
 # bash scripts/run.sh $CONFIG $NUM_GPUS
